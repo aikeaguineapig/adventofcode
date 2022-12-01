c86f1fb44cd0e7a4a8ebbf948dc46008bb1ca3d4
psyld3 <psyld3@nottingham.ac.uk>
2022-12-01 21:34:32 +0000

    Added day 1 inputs

--- adventofcodeday1/adventofcodeday1/Program.vb
+++ adventofcodeday1/adventofcodeday1/Program.vb
@@ -2,12 +2,18 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim input As String() = New String(13) {1000, 2000, 3000, "", 4000, "", 5000, 6000, "", 7000, 8000, 9000, "", 10000}
+        Dim file As String = "../../../input.txt"
+        Dim input As New ArrayList
+        FileOpen(1, file, OpenMode.Input)
+        Do While Not EOF(1)
+            input.Add(LineInput(1))
+        Loop
+        FileClose(1)
         Dim largest = 0
         Dim current
         Dim i
         Dim total = 0
-        Dim length As Integer = input.Length
+        Dim length As Integer = input.Count
         If length = 0 Then Return
         For index As Integer = 0 To length - 1
             i = index
